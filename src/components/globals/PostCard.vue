88c141aa4f83ee2fb57d5154b559ca4c1eb0de5d
Autumn Eckley <eckley.autumn@gmail.com>
2024-09-20 14:24:01 -0600

    Created account form - bootstrap template

--- src/components/globals/PostCard.vue
+++ src/components/globals/PostCard.vue
@@ -19,7 +19,7 @@ defineProps({
       <h5 class="card-title">{{ postProp.creator.name }}</h5>
       <p>{{ postProp.createdAt.toLocaleString() }}</p>
       <p class="card-text">{{ postProp.body }}</p>
-      <img class="img-fluid" :src="postProp.imgUrl" :alt="`${postProp.creator.name} posted a bad photo link`">
+      <img v-if="postProp.imgUrl" class="img-fluid" :src="postProp.imgUrl" :alt="`${postProp.creator.name} posted a bad photo link`">
       <button class="btn btn-primary">Delete if mine</button>
     </div>
     <button class="btn"><i class="mdi mdi-heart-outline">{{ postProp.likeIds.length }}</i></button>
--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -1,21 +1,25 @@
 <script setup>
 import { computed } from 'vue';
 import { AppState } from '../AppState.js';
+import AccountForm from '@/components/globals/AccountForm.vue';
 
 const account = computed(() => AppState.account)
 
 </script>
 
 <template>
-  <div class="about text-center">
-    <div v-if="account">
+  <div v-if="account">
+      <div class="about text-center">
       <h1>Welcome {{ account.name }}</h1>
       <img class="rounded" :src="account.picture" alt="" />
       <p>{{ account.email }}</p>
     </div>
-    <div v-else>
-      <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
+    <div class="p-5">
+      <AccountForm/>
     </div>
+  </div>
+  <div v-else>
+    <h1>Loading... <i class="mdi mdi-loading mdi-spin"></i></h1>
   </div>
 </template>
 
