1d238deea3b7919881c0463721786c5ab3429f43
Autumn Eckley <eckley.autumn@gmail.com>
2024-09-20 12:56:14 -0600

    Saved profile to AppState

--- src/services/ProfilesService.js
+++ src/services/ProfilesService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { Account } from "@/models/Account.js"
+import { AppState } from "@/AppState.js"
 
 class ProfilesService {
   async getProfileById(profileId) {
     const response = await api.get(`api/profiles/${profileId}`)
     logger.log('Got profile - profiles service', response.data)
+    const newProfile = new Account(response.data)
+    AppState.activeProfile = newProfile
   }
 
 }
