d61d4e32ec1e7481b61e99794577fbd562d5e1b4
Autumn Eckley <eckley.autumn@gmail.com>
2024-09-20 14:41:14 -0600

    Corrected form information, bad layout

--- src/components/globals/AccountForm.vue
+++ src/components/globals/AccountForm.vue
@@ -17,46 +17,39 @@ const editableAccountData = ref({
 <template>
 <form class="row g-3">
   <div class="col-md-6">
-    <label for="inputEmail4" class="form-label">Email</label>
-    <input type="email" class="form-control" id="inputEmail4">
+    <label for="accountName" class="form-label">Account Name</label>
+    <input v-model="editableAccountData.name" type="text" name="accountName" class="form-control" id="accountName" maxlength="100">
   </div>
   <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Password</label>
-    <input type="password" class="form-control" id="inputPassword4">
+    <label for="accountPicture" class="form-label">Profile Picture</label>
+    <input v-model="editableAccountData.picture" type="url" name="accountPicture" class="form-control" id="accountPicture" maxlength="500">
   </div>
   <div class="col-12">
-    <label for="inputAddress" class="form-label">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+    <label for="accountBio" class="form-label">Biography</label>
+    <input v-model="editableAccountData.bio" type="text" name="accountBio" class="form-control" id="accountBio" maxlength="1000">
   </div>
   <div class="col-12">
-    <label for="inputAddress2" class="form-label">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+    <label for="accountCoverImg" class="form-label">Cover Image</label>
+    <input v-model="editableAccountData.coverImg" type="url" name="accountCoverImg" class="form-control" id="accountCoverImg" maxlength="500">
   </div>
   <div class="col-md-6">
-    <label for="inputCity" class="form-label">City</label>
-    <input type="text" class="form-control" id="inputCity">
+    <label for="accountGitHub" class="form-label">GitHub Link</label>
+    <input v-model="editableAccountData.github" type="url" name="accountGitHub" class="form-control" id="accountGitHub" maxlength="500">
   </div>
-  <div class="col-md-4">
-    <label for="inputState" class="form-label">State</label>
-    <select id="inputState" class="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div class="col-md-2">
-    <label for="inputZip" class="form-label">Zip</label>
-    <input type="text" class="form-control" id="inputZip">
+  <div class="col-md-6">
+    <label for="accountLinkedIn" class="form-label">LinkedIn Link</label>
+    <input v-model="editableAccountData.linkedin" type="url" name="accountLinkedIn" class="form-control" id="accountLinkedIn" maxlength="500">
   </div>
   <div class="col-12">
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
+      <input v-model="editableAccountData.graduated" class="form-check-input" type="checkbox" name="accountGraduated" id="accountGraduated">
+      <label class="form-check-label" for="accountGraduated">
+        Have you graduated?
       </label>
     </div>
   </div>
   <div class="col-12">
-    <button type="submit" class="btn btn-primary">Sign in</button>
+    <button type="submit" class="btn btn-primary">Save Changes</button>
   </div>
 </form>
 </template>
